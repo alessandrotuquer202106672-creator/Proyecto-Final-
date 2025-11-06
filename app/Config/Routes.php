2a73e59fa3ce9004353784896f046259c91c51a8
cesar <cesarchoz@gmail.com>
2025-11-06 01:18:51 -0600

    movimiento

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,13 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+
 $routes->get('/', 'Home::index');
+
+$routes->get('reparaciones', 'ReparacionesController::index');
+$routes->post('reparaciones/agregar', 'ReparacionesController::agregarReparacion');
+$routes->get('reparaciones/eliminar/(:num)', 'ReparacionesController::eliminarReparaciones/$1');
+$routes->get('reparaciones/buscar/(:num)', 'ReparacionesController::buscarReparaciones/$1');
+$routes->post('reparaciones/modificar', 'ReparacionesController::modificarReparaciones');
+
+// ------------------------------
